ed0f7869fe55c3c68cd6ab878bfdb66ccdc76fe6
Rehman Sohan <robin.shrkr@gmail.com>
2023-09-14 16:16:12 +0600

    error handled

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
 const withMT = require("@material-tailwind/react/utils/withMT");
 const plugin = require('tailwindcss/plugin')
  
