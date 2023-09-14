74fd5bbd50089741a7001870866b1902ae60da40
Rehman Sohan <robin.shrkr@gmail.com>
2023-09-14 16:52:29 +0600

    add.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
-import NavBar from './NavBar'
-import Footer from './Footer'
+import NavBar from './Component/NavBar'
+import Footer from './Component/Footer'
 
 function App() {
 
   return (
     <div>
-      <NavBar></NavBar>
-      <Outlet></Outlet>
-      <Footer></Footer>
+      <NavBar />
+      <Outlet />
+      <Footer />
     </div>
   )
 }
